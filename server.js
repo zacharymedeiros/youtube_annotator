74bf6b0e2d366dc4a9de7cab944cddef899e61f0
zacharymedeiros <z.medeiros.rodriguez@gmail.com>
2015-12-03 14:27:33 -0800

    initial commit

--- server.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-var proxy = http.createServer(function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World');
-}).listen(1337, '127.0.0.1', function() {
-	console.log("Server running at http://127.0.0.1:1337");
-});
